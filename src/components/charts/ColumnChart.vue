b4af2f331df0b5f8422e97e8aaf4c75e2dbfeca7
Ting Hao <hao.ting@wemoscooter.com>
2023-11-15 14:43:20 +0800

    feat: add green-city board and eletricity_generation

--- src/components/charts/ColumnChart.vue
+++ src/components/charts/ColumnChart.vue
@@ -1,17 +1,22 @@
 <!-- Developed by Taipei Urban Intelligence Center 2023 -->
 
 <script setup>
-import { ref } from 'vue';
-import { useMapStore } from '../../store/mapStore';
+import { ref } from "vue";
+import { useMapStore } from "../../store/mapStore";
 
-const props = defineProps(['chart_config', 'activeChart', 'series', 'map_config']);
+const props = defineProps([
+	"chart_config",
+	"activeChart",
+	"series",
+	"map_config",
+]);
 const mapStore = useMapStore();
 
 const chartOptions = ref({
 	chart: {
 		stacked: true,
 		toolbar: {
-			show: false
+			show: false,
 		},
 	},
 	colors: props.chart_config.color,
@@ -19,12 +24,27 @@ const chartOptions = ref({
 		enabled: props.chart_config.categories ? false : true,
 		offsetY: 20,
 	},
+	// annotations: {
+	// 	yaxis: [
+	// 		{
+	// 			y: 400_000_000_000,
+	// 			borderColor: "#00E396",
+	// 			label: {
+	// 				borderColor: "#00E396",
+	// 				style: {
+	// 					color: "#fff",
+	// 					background: "#00E396",
+	// 				},
+	// 				text: "可再生能源貢獻目標",
+	// 			},
+	// 		},
+	// 	],
+	// },
 	grid: {
 		show: false,
 	},
 	legend: {
 		show: props.chart_config.categories ? true : false,
-
 	},
 	plotOptions: {
 		bar: {
@@ -32,17 +52,29 @@ const chartOptions = ref({
 		},
 	},
 	stroke: {
-		colors: ['#282a2c'],
+		colors: ["#282a2c"],
 		show: true,
 		width: 2,
 	},
 	tooltip: {
 		// The class "chart-tooltip" could be edited in /assets/styles/chartStyles.css
 		custom: function ({ series, seriesIndex, dataPointIndex, w }) {
-			return '<div class="chart-tooltip">' +
-				'<h6>' + w.globals.labels[dataPointIndex] + `${props.chart_config.categories ? '-' + w.globals.seriesNames[seriesIndex] : ''}` + '</h6>' +
-				'<span>' + series[seriesIndex][dataPointIndex] + ` ${props.chart_config.unit}` + '</span>' +
-				'</div>';
+			return (
+				'<div class="chart-tooltip">' +
+				"<h6>" +
+				w.globals.labels[dataPointIndex] +
+				`${
+					props.chart_config.categories
+						? "-" + w.globals.seriesNames[seriesIndex]
+						: ""
+				}` +
+				"</h6>" +
+				"<span>" +
+				series[seriesIndex][dataPointIndex] +
+				` ${props.chart_config.unit}` +
+				"</span>" +
+				"</div>"
+			);
 		},
 	},
 	xaxis: {
@@ -52,11 +84,13 @@ const chartOptions = ref({
 		axisTicks: {
 			show: false,
 		},
-		categories: props.chart_config.categories ? props.chart_config.categories : [],
+		categories: props.chart_config.categories
+			? props.chart_config.categories
+			: [],
 		labels: {
 			offsetY: 5,
 		},
-		type: 'category',
+		type: "category",
 	},
 });
 
@@ -68,10 +102,16 @@ function handleDataSelection(e, chartContext, config) {
 	}
 	const toFilter = config.dataPointIndex;
 	if (toFilter !== selectedIndex.value) {
-		mapStore.addLayerFilter(`${props.map_config[0].index}-${props.map_config[0].type}`, props.chart_config.map_filter[0], props.chart_config.map_filter[1][toFilter]);
+		mapStore.addLayerFilter(
+			`${props.map_config[0].index}-${props.map_config[0].type}`,
+			props.chart_config.map_filter[0],
+			props.chart_config.map_filter[1][toFilter]
+		);
 		selectedIndex.value = toFilter;
 	} else {
-		mapStore.clearLayerFilter(`${props.map_config[0].index}-${props.map_config[0].type}`);
+		mapStore.clearLayerFilter(
+			`${props.map_config[0].index}-${props.map_config[0].type}`
+		);
 		selectedIndex.value = null;
 	}
 }
@@ -79,7 +119,13 @@ function handleDataSelection(e, chartContext, config) {
 
 <template>
 	<div v-if="activeChart === 'ColumnChart'">
-		<apexchart width="100%" height="270px" type="bar" :options="chartOptions" :series="series"
-			@dataPointSelection="handleDataSelection"></apexchart>
+		<apexchart
+			width="100%"
+			height="270px"
+			type="bar"
+			:options="chartOptions"
+			:series="series"
+			@dataPointSelection="handleDataSelection"
+		></apexchart>
 	</div>
 </template>
